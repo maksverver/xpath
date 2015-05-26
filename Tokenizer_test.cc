e42515a048dee0f36335b5c7bb0a323d255fe8b0
Maks Verver <maksverver@geocities.com>
2015-05-26 21:28:57 +0200

    Add more unit tests for ScanToken().

--- Tokenizer_test.cc
+++ Tokenizer_test.cc
@@ -1,8 +1,12 @@
 #include "Tokenizer.h"
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
+#include <utility>
 
 using std::string;
+using std::vector;
+using std::pair;
 
 namespace xpath {
 namespace {
@@ -75,12 +79,59 @@ TEST(TokenizerTest, ParseTokens) {
   TestScanBasicToken("f", T_NameTest);
 }
 
+void TestTokenizer(
+    const string& input,
+    const vector<pair<TokenType,string>>& expected) {
+  int num_tokens = 0;
+  const char* input_data = input.data();
+  const char* input_end = input_data + input.size();
+  TokenType type;
+  const char* token_data;
+  size_t token_size;
+  while ((type = ScanToken(
+      input_data, input_end - input_data,
+      &token_data, &token_size)) != T_None) {
+    string token(token_data, token_data + token_size);
+    EXPECT_EQ(num_tokens < expected.size() ? expected[num_tokens].first : T_None, type);
+    EXPECT_EQ(num_tokens < expected.size() ? expected[num_tokens].second : string(), token);
+    ++num_tokens;
+    input_data = token_data + token_size;
+  }
+  EXPECT_EQ(0, token_size);
+  EXPECT_EQ(expected.size(), num_tokens);
+}
 
-// TODO: extended literal test
-// TODO: whitespace
-// TODO: several tokens in a row
-// TODO: testcase with -123 (T_MINUS, T_NUMBER)
-// TODO: testcase with more complicated sequence of tokens; check that substrings match
+TEST(TokenizerTest, MultipleTokens) {
+  TestTokenizer("concat('foo', 'bar', 'baz')",
+    {{T_NameTest, "concat"},
+     {T_LeftParen, "("},
+     {T_Literal, "'foo'"},
+     {T_Comma, ","},
+     {T_Literal, "'bar'"},
+     {T_Comma, ","},
+     {T_Literal, "'baz'"},
+     {T_RightParen, ")"}});
+  TestTokenizer(" test with \t some\nwhitespace\r",
+    {{T_NameTest, "test"},
+     {T_NameTest, "with"},
+     {T_NameTest, "some"},
+     {T_NameTest, "whitespace"}});
+  TestTokenizer("-123", {{T_Minus, "-"}, {T_Number, "123"}});
+  TestTokenizer("/child::*[position()>=1])",
+     {{T_Slash, "/"},
+      {T_NameTest, "child"},
+      {T_DoubleColon, "::"},
+      {T_Multiply, "*"},
+      {T_LeftBracket, "["},
+      {T_NameTest, "position"},
+      {T_LeftParen, "("},
+      {T_RightParen, ")"},
+      {T_GreaterEqual, ">="},
+      {T_Number, "1"},
+      {T_RightBracket, "]"},
+      {T_RightParen, ")"}});
+}
+
 // TODO: unit tests for DisambiguateToken
 
 }  // namespace xpath
